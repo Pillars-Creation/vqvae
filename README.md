aa46d2063e329908f5c81853f764673c4f2c824c
qjzcyzhang <qjzcyzhang@tencent.com>
2023-10-20 11:03:37 +0800

    add dalle

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # VQVAE
 
 运行
-python main.py --warmup_model vqvae
+python main.py --warmup_model base
 
 ![img_1.png](img_1.png)
 
+python main.py --warmup_model vqvae
+
 ![img.png](img.png)
+
+python main.py --warmup_model dalle
+
+![img_2.png](img_2.png)
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--pretrain_model', default='deepfm')
     parser.add_argument('--dataset_name', default='movielens1M', help='required to be one of [movielens1M, taobaoAd]')
-    parser.add_argument('--dataset_path', default='./datahub/movielens1M/ml-1M.pkl')
+    parser.add_argument('--dataset_path', default='./datahub/movielens1M/movielens1M_data.pkl')
     parser.add_argument('--warmup_model', default='vqvae', help="required to be one of [base, mwuf, metaE, cvar, vqvae, dalle]")
     parser.add_argument('--is_dropoutnet', type=bool, default=False, help="whether to use dropout net for pretrain")
     parser.add_argument('--bsz', type=int, default=2048)
